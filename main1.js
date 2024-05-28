decb38cd9963fd7704ee0b223868af1aa019f0c1
vinothtimes1 <vinothtimes@gmail.com>
2024-05-28 22:47:34 +0530

    hfindex
    index comp

--- main1.js
+++ main1.js
@@ -5,259 +5,25 @@ function myFunction() {
 }
 
 //alert(2)
-class myHeader extends HTMLElement{
+
+
+class mainHeader extends HTMLElement{
 
 connectedCallback()
 {
-//alert(21)	
+//alert(21)
+
+
+alert("header executed");	
 this.innerHTML=`
 
-<header>
 	
-	
-									<section class="elementor-section elementor-top-section elementor-element elementor-element-0d991fe elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="0d991fe" data-element_type="section">
-						<div class="elementor-container elementor-column-gap-default">
-							<div class="elementor-row">
-					<div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-0dd8647" data-id="0dd8647" data-element_type="column">
-			<div class="elementor-column-wrap elementor-element-populated">
-							<div class="elementor-widget-wrap">
-						<div class="left elementor-element elementor-element-03b433f elementor-widget elementor-widget-scp_menu" data-id="03b433f" data-element_type="widget" data-widget_type="scp_menu.default">
-				<div class="elementor-widget-container">
-			<div id="wh-mobile-menu" class="respmenu-wrap">
-	<div class="respmenu-header">
-					<a href="index.htm" class="respmenu-header-logo-link">
-				<img src="app/uploads/sites/4/2019/08/ed-logo-grey1.png" class="respmenu-header-logo" alt="mobile-logo">
-			</a>
-				<div class="respmenu-open">
-					<hr>
-			<hr>
-			<hr>
-				</div>
-	</div>
-
-
-
-	<div class="menu-mobile-menu-container"><ul id="menu-mobile-menu-1" class="respmenu">
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-1530 current_page_item menu-item-2395 msm-top-level-item"><a href="index.htm" aria-current="page">Home</a></li>
-
-	
-	
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-1887 msm-top-level-item">
-<a href="modern/about-us/index.htm">About Us</a>
-
-</li>
-
-
-<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1906"><a href="modern/teachers-page/index.htm">Teachers</a></li>
-
-	
-<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-1889 msm-top-level-item"><a href="modern/alumni/index.htm">Alumni</a>
-</li>
-	
-
-<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-1889 msm-top-level-item"><a href="modern/gallery/index.htm">Gallery</a>
-</li>
-
-	
-
-<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-1887 msm-top-level-item">
-<a href="modern/student/index.htm">Student</a>
-<div class="respmenu-submenu-toggle cbp-respmenu-more"><i class="icon-edright-arrow"></i></div>
-
-<ul class="sub-menu">
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1906"><a href="modern/uniforms/index.htm">Uniforms</a></li>
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1905"><a href="modern/schoolhours/index.htm">School Hours</a></li>
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1905"><a href="modern/academiccalender/index.htm">Academic Calender</a></li>
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1905"><a href="modern/holidays/index.htm">Holidays</a></li>
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1905"><a href="modern/studentcodeofconduct/index.htm">Student Code of Conduct</a></li>
-</ul>
-</li>
-
-
-
-
-<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-1887 msm-top-level-item">
-<a href="modern/academiccalender/index.htm">Events</a>
-<div class="respmenu-submenu-toggle cbp-respmenu-more"><i class="icon-edright-arrow"></i></div>
-
-<ul class="sub-menu">
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1906"><a href="modern/previousevents/index.htm">Previous Events</a></li>
-</ul>
-</li>
-
-
-
-
-<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2382 msm-top-level-item"><a href="modern/contact/index.htm">Contact</a></li>
-
-
-
-
-</div></div>
-		</div>
-				</div>
-						</div>
-					</div>
-		</div>
-								</div>
-					</div>
-		</section>
-									</div>
-			</div>
-					</div>
-			</div>
-
-
-
-		<!-- main menu -->
-
-
-
-			<div class="cbp-row wh-header top wh-sticky-header-enabled">
-				<div data-elementor-type="wp-post" data-elementor-id="1533" class="elementor elementor-1533">
-						<div class="elementor-inner">
-				<div class="elementor-section-wrap">
-				
-				
-				
-				<section class="elementor-section elementor-top-section elementor-element elementor-element-f985d2d elementor-section-stretched elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="f985d2d" data-element_type="section" data-settings="{&quot;stretch_section&quot;:&quot;section-stretched&quot;}">
-						
-<style>
-            .blink {
-               color: red;
-                font-family: sans-serif;
-				align: center;
-				
-				
-            }
-            
-        </style>				
-	
-	
-			
-		</section>
-		
-	<marquee class="blink" bgcolor="#66b2ff">Admissions Open From PRE-KG to VIII std</marquee>
-		
-				
-				
-				
-				
-									<section class="elementor-section elementor-top-section elementor-element elementor-element-f985d2d elementor-section-stretched elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="f985d2d" data-element_type="section" data-settings="{&quot;stretch_section&quot;:&quot;section-stretched&quot;}">
-						<div class="elementor-container elementor-column-gap-default">
-							<div class="elementor-row">
-					<div class="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-b6dc7ce width-auto" data-id="b6dc7ce" data-element_type="column">
-			<div class="elementor-column-wrap elementor-element-populated">
-							<div class="elementor-widget-wrap">
-						<div class="elementor-element elementor-element-1bb33ca elementor-widget elementor-widget-scp_logo" data-id="1bb33ca" data-element_type="widget" data-widget_type="scp_logo.default">
-				<div class="elementor-widget-container">
-
-	
-			<div class="elementor-image"><a href="index.htm"><img src="app/uploads/sites/4/2019/08/ed-logo-grey.png" alt="logo"></a></div>		</div>
-				</div>
-						</div>
-					</div>
-		</div>
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-				<div class="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-9853566 width-auto margin-left-auto" data-id="9853566" data-element_type="column">
-			<div class="elementor-column-wrap elementor-element-populated">
-							<div class="elementor-widget-wrap">
-						<div class="right elementor-element elementor-element-b0a2c5a elementor-widget elementor-widget-scp_menu" data-id="b0a2c5a" data-element_type="widget" data-widget_type="scp_menu.default">
-				<div class="elementor-widget-container">
-			<div class="menu-top-menu-container"><ul id="menu-top-menu-1" class="sf-menu wh-menu-top right"><!-- <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-230 msm-top-level-item"><a href="#">Parents</a></li>
-<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1817 msm-top-level-item"><a href="#">Staff</a></li> -->
-<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1818 msm-top-level-item"><img src="app/uploads/sites/4/2019/08/phone.png" alt="logo"><span>&nbsp;&nbsp; 0416-290-7171</span>	
-				</a>
-				</li>
-				
-				
-</ul></div>		</div>
-				</div>
-						</div>
-					</div>
-		</div>
-				<div class="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-e8351c4 width-auto " data-id="e8351c4" data-element_type="column">
-			<div class="elementor-column-wrap elementor-element-populated">
-							<div class="elementor-widget-wrap">
-						<div class="elementor-element elementor-element-7644782 elementor-align-right hoverable elementor-widget elementor-widget-button" data-id="7644782" data-element_type="widget" data-widget_type="button.default">
-				<div class="elementor-widget-container">
-					<div class="elementor-button-wrapper">
-			<a href="modern/donate/index.htm" class="elementor-button-link elementor-button elementor-size-md" role="button">
-						<span class="elementor-button-content-wrapper">
-						<span class="elementor-button-text">DONATE</span>
-		</span>
-					</a>
-		</div>
-				</div>
-				</div>
-						</div>
-					</div>
-		</div>
-		
-		
-<!-- 				<div class="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-9853566 width-auto margin-left-auto" data-id="9853566" data-element_type="column">
-			<div class="elementor-column-wrap elementor-element-populated">
-							<div class="elementor-widget-wrap">
-						<div class="right elementor-element elementor-element-b0a2c5a elementor-widget elementor-widget-scp_menu" data-id="b0a2c5a" data-element_type="widget" data-widget_type="scp_menu.default">
-				<div class="elementor-widget-container">
-				<div class="elementor-image"><img src="app/uploads/sites/4/2019/08/phone.png" alt="logo">	
-				
-			</div>		</div>	
-				
-		</div>
-				</div>
-						</div>
-					</div>
-		</div>
-				 <div class="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-e8351c4 width-auto " data-id="e8351c4" data-element_type="column">
-			<div class="elementor-column-wrap elementor-element-populated">
-							<div class="elementor-widget-wrap">
-						<div class="elementor-element elementor-element-7644782 elementor-align-right hoverable elementor-widget elementor-widget-button" data-id="7644782" data-element_type="widget" data-widget_type="button.default">
-				<div class="elementor-widget-container">
-					<div class="elementor-button-wrapper">
-					
-				<span class="elementor-button-text">0416-0000-000</span>	
-		</div>
-				</div>
-				</div>
-						</div>
-					</div>
-		</div> 
- -->		
-		
-		
+	<header>	
 			
 		
-								</div> 
-					</div>
-		</section>
 		
 		
-		
-		
-				<section class="elementor-section elementor-top-section elementor-element elementor-element-98212d1 elementor-section-stretched elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="98212d1" data-element_type="section" data-settings="{&quot;stretch_section&quot;:&quot;section-stretched&quot;}">
-						<div class="elementor-container elementor-column-gap-default">
-							<div class="elementor-row">
-					<div class="elementor-column elementor-col-66 elementor-top-column elementor-element elementor-element-1f7529c" data-id="1f7529c" data-element_type="column">
-			<div class="elementor-column-wrap elementor-element-populated">
-							<div class="elementor-widget-wrap">
-						<div class="right elementor-element elementor-element-22061b4 elementor-widget elementor-widget-scp_menu" data-id="22061b4" data-element_type="widget" data-widget_type="scp_menu.default">
-				<div class="elementor-widget-container">
-			<div id="cbp-menu-main" class="menu-main-menu-container"><ul id="menu-main-menu-1" class="sf-menu wh-menu-main left">
+						<ul id="menu-main-menu-1" class="sf-menu wh-menu-main left">
 			<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-1530 current_page_item menu-item-2396 msm-top-level-item"><a href="index.htm" aria-current="page">Home</a></li>
 <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-1530 current_page_item menu-item-2396 msm-top-level-item"><a href="modern/about-us/index.htm">About Us</a>
 </li>
@@ -393,48 +159,8 @@ this.innerHTML=`
 					</div>
 		</div>
 </li>
-</ul></div>		</div>
-				</div>
-						</div>
-					</div>
-		</div>
-		 		<div class="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-a6a2353 width-auto margin-left-auto" data-id="a6a2353" data-element_type="column">
-			<div class="elementor-column-wrap elementor-element-populated">
-		 			<div class="elementor-widget-wrap">
-						<div class="elementor-element elementor-element-730b789 elementor-shape-circle e-grid-align-right elementor-grid-0 elementor-widget elementor-widget-social-icons" data-id="730b789" data-element_type="widget" data-widget_type="social-icons.default">
-				<div class="elementor-widget-container">
-					<div class="elementor-social-icons-wrapper elementor-grid">
-							<span class="elementor-grid-item">
-					<a class="elementor-icon elementor-social-icon elementor-social-icon-facebook-f elementor-repeater-item-240e416" href="https://www.facebook.com/profile.php?id=1000077027487560" target="_blank">
-						<span class="elementor-screen-only">Facebook-f</span>
-						<i class="fab fa-facebook-f"></i>					</a>
-				</span>
-							<span class="elementor-grid-item">
-					<a class="elementor-icon elementor-social-icon elementor-social-icon-twitter elementor-repeater-item-b9443df" href="https://www.twitter.com/siddhartha_scl" target="_blank">
-						<span class="elementor-screen-only">Twitter</span>
-						<i class="fab fa-twitter"></i>					</a>
-				</span>
-							<span class="elementor-grid-item">
-					<a class="elementor-icon elementor-social-icon elementor-social-icon-instagram elementor-repeater-item-eb9b1ee" href="https://www.instagram.com/siddharthaschoolsvellore" target="_blank">
-						<span class="elementor-screen-only">Instagram</span>
-						<i class="fab fa-instagram"></i>					</a>
-				</span>
-							<span class="elementor-grid-item">
-					<a class="elementor-icon elementor-social-icon elementor-social-icon-youtube elementor-repeater-item-3bcd036" href="https://www.youtube.com/@siddharthaschools" target="_blank">
-						<span class="elementor-screen-only">Youtube</span>
-						<i class="fab fa-youtube"></i>					</a>
-				</span>
-					</div>
-				</div>
-				</div>
-						</div>
-				 	</div>
-		 </div> 
-		 						</div>
-					</div>
-		</section>
-	
-
+</ul>
+		
 
 	</header>
 
@@ -443,7 +169,7 @@ this.innerHTML=`
 
 }
 }
-customElements.define('my-header', myHeader)
+customElements.define('main-header', mainHeader)
 
 
 
@@ -469,3 +195,88 @@ this.innerHTML=`
 }
 customElements.define('my-footer', myFooter)
 
+
+
+
+
+class mobileHeader extends HTMLElement{
+
+connectedCallback()
+{
+//alert(21)
+
+
+alert("header executed");	
+this.innerHTML=`
+
+<header>
+	
+	
+									
+		<ul id="menu-mobile-menu-1" class="respmenu">
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-1530 current_page_item menu-item-2395 msm-top-level-item"><a href="index.htm" aria-current="page">Home</a></li>
+
+	
+	
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-1887 msm-top-level-item">
+<a href="modern/about-us/index.htm">About Us</a>
+
+</li>
+
+
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1906"><a href="modern/teachers-page/index.htm">Teachers</a></li>
+
+	
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-1889 msm-top-level-item"><a href="modern/alumni/index.htm">Alumni</a>
+</li>
+	
+
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-1889 msm-top-level-item"><a href="modern/gallery/index.htm">Gallery</a>
+</li>
+
+	
+
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-1887 msm-top-level-item">
+<a href="modern/student/index.htm">Student</a>
+<div class="respmenu-submenu-toggle cbp-respmenu-more"><i class="icon-edright-arrow"></i></div>
+
+<ul class="sub-menu">
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1906"><a href="modern/uniforms/index.htm">Uniforms</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1905"><a href="modern/schoolhours/index.htm">School Hours</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1905"><a href="modern/academiccalender/index.htm">Academic Calender</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1905"><a href="modern/holidays/index.htm">Holidays</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1905"><a href="modern/studentcodeofconduct/index.htm">Student Code of Conduct</a></li>
+</ul>
+</li>
+
+
+
+
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-1887 msm-top-level-item">
+<a href="modern/academiccalender/index.htm">Events</a>
+<div class="respmenu-submenu-toggle cbp-respmenu-more"><i class="icon-edright-arrow"></i></div>
+
+<ul class="sub-menu">
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1906"><a href="modern/previousevents/index.htm">Previous Events</a></li>
+</ul>
+</li>
+
+
+
+
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2382 msm-top-level-item"><a href="modern/contact/index.htm">Contact</a></li>
+
+
+</ul>
+
+
+
+
+</header>
+
+	
+	`
+
+}
+}
+customElements.define('mobile-header', mobileHeader)
